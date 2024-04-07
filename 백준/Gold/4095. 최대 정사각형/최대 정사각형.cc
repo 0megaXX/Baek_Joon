f79c218595f09e91f0fe8a9c9a9c56e5b34bae1a
0megaXX <145690298+0megaXX@users.noreply.github.com>
2024-04-07 13:18:03 +0900

    [Gold IV] Title: 최대 정사각형, Time: 868 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Gold/4095. 최대 정사각형/최대 정사각형.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <stddef.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <sstream>
+#include <cmath>
+#include<map>
+#include <queue>
+#include <stack>
+#include <cstring> 
+#include <deque>
+#include <set>
+
+
+using namespace std;
+
+int arr[1001][1001];
+int dp[1001][1001];
+
+int row, col;
+
+
+
+int main(void)
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	
+	cin >> row >> col;
+	while (!(row == 0 && col == 0))
+	{
+
+		memset(dp, 0, sizeof(dp));
+		memset(arr, 0, sizeof(arr));
+		for (int a = 1; a <= row; a++)
+		{
+			for (int b = 1; b <= col; b++)
+			{
+				cin >> arr[a][b];
+				if (arr[a][b] == 1)
+					dp[a][b] = 1;
+			}
+		}
+		int maxx = 0;
+		for (int a = 1; a <= row; a++)
+		{
+			for (int b = 1; b <= col; b++)
+			{
+				if (arr[a][b] == 1)
+				{
+					dp[a][b] = min({ dp[a - 1][b],dp[a][b - 1],dp[a - 1][b - 1] }) + dp[a][b];
+					maxx = max(maxx, dp[a][b]);
+
+				}
+			}
+		}
+		cout << maxx<<'\n';
+		cin >> row >> col;
+	}
+	/*cout << endl;
+	for (int a = 1; a <= row; a++)
+	{
+		for (int b = 1; b <= col; b++)
+		{
+			cout << dp[a][b] << ' ';
+		}
+		cout<<endl;
+	}*/
+}
