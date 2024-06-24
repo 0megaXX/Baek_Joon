e416cbfdc696dcd16080185028559f86d74bfd89
0megaXX <145690298+0megaXX@users.noreply.github.com>
2024-06-24 11:36:07 +0900

    [Gold III] Title: 전기가 부족해, Time: 36 ms, Memory: 4464 KB -BaekjoonHub

---
+++ 백준/Gold/10423. 전기가 부족해/전기가 부족해.cc
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <stddef.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <sstream>
+#include <cmath>
+#include<map>
+#include <queue>
+#include <stack>
+#include <cstring> 
+#include <deque>
+#include <set>
+
+using namespace std;
+
+
+int N, M, K;
+vector<pair<int, pair<int, int>>> v;
+
+int parent[1001];
+
+
+int find_parent(int a)
+{
+
+	if (parent[a] == a)
+		return a;
+	else
+		return parent[a] = find_parent(parent[a]);
+}
+
+void union_parent(int a, int b)
+{
+
+	a = find_parent(a);
+	b = find_parent(b);
+
+	if (a < b)
+		parent[b] = a;
+	else
+		parent[a] = b;
+
+}
+
+
+int main(void)
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M>>K;
+
+	for (int a = 1; a <= N; a++)
+		parent[a] = a;
+
+	vector<int>fac;
+	for (int a = 0; a < K; a++)
+	{
+		int b;
+		cin >> b;
+		fac.push_back(b);
+	}
+
+	int center = fac[0];
+
+	for (int a = 1; a < K; a++)
+		union_parent(center, fac[a]);
+
+
+
+	for (int a = 0; a < M; a++)
+	{
+		int start, end, cost;
+		cin >> start >> end >> cost;
+		v.push_back({ cost,{start,end} });
+	}
+
+	sort(v.begin(), v.end());
+
+	int summ = 0;
+
+	for (int a = 0; a < M; a++)
+	{
+		int cost = v[a].first;
+		int start = v[a].second.first;
+		int end = v[a].second.second;
+
+
+		if (find_parent(start) != find_parent(end))//연결이 안되어있다면
+		{
+			summ += cost;
+			union_parent(start, end);
+		}
+	}
+	cout << summ;
+	
+}
