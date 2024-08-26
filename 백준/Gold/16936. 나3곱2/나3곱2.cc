ad920e43656d33f824ec1f9bcd1a4afc9cedaf76
0megaXX <145690298+0megaXX@users.noreply.github.com>
2024-08-26 17:25:06 +0900

    [Gold V] Title: 나3곱2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/16936. 나3곱2/나3곱2.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <stddef.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <sstream>
+#include <cmath>
+#include<map>
+#include <queue>
+#include <stack>
+#include <cstring> 
+#include <deque>
+#include <set>
+#include <unordered_set>
+
+#define MAXX 987654321
+using namespace std;
+
+vector<unsigned long long> v;
+bool visited[101];
+int n;
+
+void Solve(unsigned long long start)
+{
+
+    vector<unsigned long long> ans;
+    ans.push_back(start);
+
+    for (int a = 1; a < n; a++) 
+        for (int b = 0; b < n; b++) 
+        {
+            if (ans.back() % 3 == 0 && v[b] == ans.back() / 3) // 배열 맨 끝이 3으로 나눠지고 3으로 나눌때 현재 위치의 값이라면
+                ans.push_back(v[b]);
+            
+            else if (v[b] == ans.back() * 2) //배열 맨끝의 값 * 2가 현재 위치의 값이라면
+                ans.push_back(v[b]);
+            
+        }
+
+
+    if (ans.size() == n) // 조합이 완성되었다면
+    {
+        for (int k = 0; k < n; k++)
+            cout << ans[k] << " ";
+        exit(0);
+    }
+
+    else
+        return;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+   
+    for (int a = 0; a < n; a++)
+    {
+        unsigned long long b;
+        cin >> b;
+        v.push_back(b);
+    }
+
+    sort(v.begin(), v.end());
+    for (int a = 0; a < n; a++) 
+        Solve(v[a]);
+}
