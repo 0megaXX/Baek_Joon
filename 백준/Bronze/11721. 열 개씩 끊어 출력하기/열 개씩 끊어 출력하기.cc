4938b405fac7e5d374d9f6bfe2c1728d7520f32a
0megaXX <145690298+0megaXX@users.noreply.github.com>
2024-09-17 06:32:12 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <stddef.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <sstream>
+#include <cmath>
+#include<map>
+#include <queue>
+#include <stack>
+#include <cstring> 
+#include <deque>
+#include <set>
+#include <unordered_set>
+
+#define MAXX 987654321
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+    cin >> s;
+    for (int a = 0; a < s.size(); a++)
+    {
+ 
+            cout << s[a];
+            if(a!=0 && a%10==9)
+               
+        cout << '\n';
+    }
+    return 0;
+}
