05acaa22d0569fa3ded508fe6defa6b69667ff20
0megaXX <145690298+0megaXX@users.noreply.github.com>
2024-09-21 09:05:30 +0900

    [Bronze II] Title: 줄 세우기, Time: 20 ms, Memory: 13744 KB -BaekjoonHub

---
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <stddef.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <sstream>
+#include <cmath>
+#include<map>
+#include <queue>
+#include <stack>
+#include <cstring> 
+#include <deque>
+#include <set>
+#include <unordered_set>
+
+#define MAXX 987654321
+using namespace std;
+
+
+vector<int>v(1000000);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int cass;
+	cin >> cass;
+
+	for (int a = 0; a < cass; a++)
+	{
+		vector<int>::iterator it = v.begin();
+		int b;
+		cin >> b;
+
+		v.insert(it+( a - b), a+1);
+
+	}
+	for (int a = 0; a < cass; a++)
+		cout << v[a] << ' ';
+	return 0;
+}
