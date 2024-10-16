a64b6ef8750e3749f95ef8046586cd8d3ced3293
0megaXX <145690298+0megaXX@users.noreply.github.com>
2024-10-16 14:14:55 +0900

    [Gold I] Title: 커피숍2, Time: 116 ms, Memory: 33276 KB -BaekjoonHub

---
+++ 백준/Gold/1275. 커피숍2/커피숍2.cc
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <stddef.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <sstream>
+#include <cmath>
+#include<map>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <chrono>
+
+#define MAXX 1000000000
+using namespace std;
+int N, Q;
+
+vector<long long > v(1000001);
+vector<long long> tree(3000001);
+
+// 구간 트리 초기화
+long long init(int node, int start, int end)
+{
+    if (start == end) 
+        return tree[node] =v[start];
+    
+    int mid = (start + end) / 2;
+    long long left = init(2 * node, start, mid);
+    long long right = init(2 * node + 1, mid + 1, end);
+
+    return tree[node] =  left+right;
+}
+
+// 구간 트리에서 쿼리
+long long query(int node, int start, int end, int left, int right)
+{
+    if (right < start || end < left)
+        return 0 ; // 범위 밖
+
+    if (left <= start && end <= right)
+        return tree[node]; // 범위 안
+
+    int mid = (start + end) / 2;
+    long long l = query(2 * node, start, mid, left, right);
+    long long r = query(2 * node + 1, mid + 1, end, left, right);
+
+ 
+    return l + r;
+}
+
+
+// 세그먼트 트리 재구축
+long long re_init(int node, int start, int end, int target)
+{
+
+
+    if (start == end) // 도착했다면
+        return tree[node]=v[target];
+
+    int mid = (start + end) / 2;
+
+    if (target > mid) // 찾는 위치가 중앙보다 크다면
+        return tree[node] = tree[node * 2] + re_init(node * 2 + 1, mid + 1, end, target) ;
+
+    else  //찾는 위치가 중앙보다 작거나 같다면 
+        return tree[node] = re_init(node * 2, start,mid, target) + tree[node * 2+1] ;
+
+   
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> Q;
+
+
+    for (int a = 0; a < N; a++)
+        cin >> v[a];
+    
+
+    // 구간 트리 초기화
+    init(1, 0, N - 1);
+    
+
+    for (int a = 0; a < Q ; a++)
+    {
+        
+        
+        int b,c;
+        cin >> b >> c;
+     
+        if (b < c)
+            cout << query(1, 0, N - 1, b - 1, c - 1) << '\n';
+        else
+            cout << query(1, 0, N - 1, c - 1, b - 1) << '\n';
+
+        long long d, e;
+        cin >> d >> e;      
+        v[d-1] = e;
+        re_init(1, 0, N - 1, d - 1);
+        
+   
+
+    }
+
+    return 0;
+}
