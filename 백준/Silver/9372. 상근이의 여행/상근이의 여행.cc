5272a7d141c80147b95d4f2fb6b2a0101e807f86
0megaXX <145690298+0megaXX@users.noreply.github.com>
2024-11-15 17:41:22 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,89 @@
+
+#include <iostream>
+
+#include <string>
+
+#include <vector>
+
+#include <ctime>
+
+#include <cstdlib>
+
+#include <fstream>
+
+#include <stddef.h>
+
+#include<iostream>
+
+#include<string>
+
+#include<algorithm>
+
+#include<vector>
+
+#include <sstream>
+
+#include <cmath>
+
+#include<map>
+
+#include <queue>
+
+#include <stack>
+
+#include <cstring>
+
+#include <deque>
+
+#include <set>
+
+#include <unordered_set>
+
+#include <chrono>
+
+#define MAXX 1000000000
+
+using namespace std;
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+
+    cin.tie(NULL);
+
+    cout.tie(NULL);
+
+    int cass;
+
+    cin >> cass;
+
+    while (cass--)
+
+    {
+
+        int N, M;
+
+        cin >> N >> M;
+
+        for (int a = 0; a < M; a++)
+
+        {
+
+            int b, c;
+
+            cin >> b >> c;
+
+        }
+
+        cout << N - 1 << '\n';
+
+       
+
+    }
+
+ 
+
+    return 0;
+
+}
+
