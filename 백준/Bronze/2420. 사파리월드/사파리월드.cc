a1b8c3c112ca11ea48440b12cf5951bee62778b7
0megaXX <145690298+0megaXX@users.noreply.github.com>
2024-11-22 19:04:26 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <stddef.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <sstream>
+#include <cmath>
+#include<map>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <chrono>
+
+#define MAXX 1000000000
+using namespace std;
+
+int arr[7] = { 1, 1, 2, 2, 2, 8 };
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    long long n, m;
+    cin >> n >> m;
+    cout << abs(n - m);
+}
