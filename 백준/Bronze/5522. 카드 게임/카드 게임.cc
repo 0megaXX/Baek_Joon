082a47752f699300e5b420e824d8046a24927851
0megaXX <145690298+0megaXX@users.noreply.github.com>
2025-08-25 00:52:10 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <stddef.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <sstream>
+#include <cmath>
+#include<map>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <chrono>
+#include <tuple>
+#include <regex>
+
+using namespace std;
+#define MAXX 2147483647
+
+
+
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+	int summ = 0;
+	int n;
+	for (int a = 0; a < 5; a++) 
+	{
+		cin >> n;
+		summ += n;
+	}
+
+	cout << summ;
+
+    return 0;
+}
