e07b32b80cc3106b545dedfc8d5a7ea67a5d2812
0megaXX <145690298+0megaXX@users.noreply.github.com>
2026-01-03 22:10:56 +0900

    [Silver III] Title: 내 선물을 받아줘 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15886. 내 선물을 받아줘 2/내 선물을 받아줘 2.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <stddef.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <sstream>
+#include <cmath>
+#include<map>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <chrono>
+#include <tuple>
+#include <regex>
+
+using namespace std;
+#define MAXX 2147483647
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    string s;
+
+    cin >> n >>s;
+    int rs = 0;
+   
+    for (int a = 0; a < n - 1; a++)
+        if (s[a] == 'E' && s[a + 1] == 'W')
+            rs++;
+   
+    cout << rs;
+   
+    return 0;
+}
