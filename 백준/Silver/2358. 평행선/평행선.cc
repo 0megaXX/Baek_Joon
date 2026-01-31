cb98549a8f58f0cdeb6e7c0a340f1755ba9c1465
0megaXX <145690298+0megaXX@users.noreply.github.com>
2026-01-31 17:47:47 +0900

    [Silver IV] Title: 평행선, Time: 64 ms, Memory: 6908 KB -BaekjoonHub

---
+++ 백준/Silver/2358. 평행선/평행선.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <stddef.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <sstream>
+#include <cmath>
+#include<map>
+#include <queue>
+#include <stack>
+#include <cstring>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <chrono>
+#include <tuple>
+#include <regex>
+
+using namespace std;
+#define MAXX 2147483647
+
+map <int,int> xm;
+map <int,int> ym;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+   
+    cin >> n;
+   
+    for(int a = 0; a < n; a++)
+    {
+        int x, y;
+        cin >> x >> y;
+        xm[x]++;
+        ym[y]++;
+    }
+
+    int rs = 0;
+   
+    for(auto it : xm)
+      if(it.second >= 2)
+          rs++;
+   
+     for(auto it : ym)
+        if(it.second >= 2)
+            rs++;
+     
+    cout << rs;
+	
+
+    return 0;
+}
